2f950bcb736519f6035990e03afe989fe6a0e78b
Keisuke Takeuchi <142223934+neji-craftsman@users.noreply.github.com>
2024-11-29 10:39:37 +0900

    Enhance documentation for simulating function creation from ODE RHS (#4)

--- nasap/simulation/simulating_func.py
+++ nasap/simulation/simulating_func.py
@@ -1,3 +1,7 @@
+"""
+Module for making simulating functions from ODE right-hand side functions.
+"""
+
 from collections.abc import Callable
 from typing import Concatenate, ParamSpec
 
@@ -11,25 +15,40 @@ def make_simulating_func_from_ode_rhs(
         ) -> Callable[Concatenate[npt.NDArray, npt.NDArray, _P], npt.NDArray]:
     """Make a simulating function from an ODE right-hand side function.
 
-    Resulting simulating function can be called with time points, initial
-    values of the dependent variables, and the parameters of the ODE right-hand
-    side function, e.g., `simulating_func(t, y0, k1, k2, k3, k4)` for a
-    4-parameter ODE right-hand side function `ode_rhs(t, y, k1, k2, k3, k4)`.
+    Resulting simulating function can be called with time points, 
+    initial values of the dependent variables, and the parameters of 
+    the ODE right-hand side function. For example, it can be called
+    as ``simulating_func(t, y0, k1, k2, k3, k4)`` for a 4-parameter 
+    ODE right-hand side function ``ode_rhs(t, y, k1, k2, k3, k4)``.
 
     Parameters
     ----------
     ode_rhs: Callable
-        The ODE right-hand side function, e.g., `ode_rhs(t, y, k1, k2)`.
-        The first argument should be the time point (float), the second
-        argument should be the dependent variables (npt.NDArray), 
-        and the rest of the arguments should be the parameters of the 
-        ODE right-hand side function (float). The names of `t` and `y`
-        can be arbitrary.
+        The ODE right-hand side function. It should have the signature
+        
+            ``ode_rhs(t, y, *args, **kwargs) -> dydt``
+
+        where ``t`` is the time (float), ``y`` is the dependent
+        variable (1-D array, shape (n,)), ``args`` and ``kwargs``
+        are the parameters of the ODE right-hand side function, and
+        ``dydt`` is the derivative of the dependent variable (1-D
+        array, shape (n,)). The names of ``t`` and ``y`` can be
+        different.
 
     Returns
     -------
     Callable
         The simulating function for the ODE right-hand side.
+        It has the signature
+        
+            ``simulating_func(t, y0, *args, **kwargs) -> y``
+    
+        where ``t`` is the time points (1-D array, shape (m,)),
+        ``y0`` is the initial values of the dependent variables
+        (1-D array, shape (n,)), ``args`` and ``kwargs`` are the
+        parameters of the ODE right-hand side function, and ``y``
+        is the dependent variables at the time points (2-D array,
+        shape (m, n)).
     """
     def simulating_func(
             t: npt.NDArray, y0: npt.NDArray,
