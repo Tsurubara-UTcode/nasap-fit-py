795efee47796f8e7b2f077e2f932db9db7af87ce
Keisuke Takeuchi <142223934+neji-craftsman@users.noreply.github.com>
2024-11-29 11:48:25 +0900

    Refactor RSS calculation function to receive `args` and `kwargs` instead of `params_d` (#5)

--- nasap/fitting/rss.py
+++ nasap/fitting/rss.py
@@ -1,18 +1,21 @@
 """Module for calculating the residual sum of squares (RSS) between the
 data and the simulation."""
 
-from collections.abc import Callable, Mapping
-from typing import Concatenate
+from collections.abc import Callable
+from typing import Concatenate, ParamSpec
 
 import numpy as np
 import numpy.typing as npt
 
+_P = ParamSpec('_P')
 
+# TODO: Add examples
 def calc_simulation_rss(
         tdata: npt.NDArray, ydata: npt.NDArray, 
         simulating_func: Callable[
-            Concatenate[npt.NDArray, npt.NDArray, ...], npt.NDArray],
-        y0: npt.NDArray, params_d: Mapping[str, float],
+            Concatenate[npt.NDArray, npt.NDArray, _P], npt.NDArray],
+        y0: npt.NDArray, 
+        *args: _P.args, **kwargs: _P.kwargs
         ) -> float:
     """Calculate the residual sum of squares (RSS) between the data 
     and the simulation.
@@ -27,16 +30,18 @@ def calc_simulation_rss(
     simulating_func : Callable
         Function that simulates the system. 
 
-            ``simulating_func(t, y0, **params_d) -> y``
+            ``simulating_func(t, y0, *args, **kwargs) -> y``
 
         where ``t`` is a 1-D array with shape (n,), ``y0`` is a 1-D
-        array with shape (m,), ``params_d`` is a dictionary with the
-        parameters of the system, and ``y`` is a 2-D array with shape
-        (n, m).
+        array with shape (m,), ``args`` and ``kwargs`` are additional
+        arguments and keyword arguments for the simulation, and ``y``
+        is a 2-D array with shape (n, m).
     y0 : npt.NDArray, shape (m,)
         Initial conditions.
-    params_d : Mapping[str, float]
-        Parameters of the system.
+    *args
+        Additional arguments and keyword arguments for the simulation.
+    **kwargs
+        Additional arguments and keyword arguments for the simulation.
 
     Returns
     -------
@@ -50,7 +55,7 @@ def calc_simulation_rss(
     In the case where some species are not measured, the corresponding
     values in `ydata` should be NaN, to keep the shape consistent.
     """
-    ysim = simulating_func(tdata, y0, **params_d)
+    ysim = simulating_func(tdata, y0, *args, **kwargs)
 
     # Calculate the residuals
     residuals = ysim - ydata  # can include NaN values
--- nasap/fitting/tests/test_rss.py
+++ nasap/fitting/tests/test_rss.py
@@ -2,6 +2,7 @@ from unittest.mock import Mock
 
 import numpy as np
 import pytest
+from scipy.optimize import basinhopping, differential_evolution
 
 from nasap.fitting import calc_simulation_rss
 from nasap.simulation import make_simulating_func_from_ode_rhs
@@ -19,9 +20,8 @@ def test_zero_rss():
     k = 1
 
     ydata = simulating_func(tdata, y0, k)
-    params_d = {'k': k}
 
-    rss = calc_simulation_rss(tdata, ydata, simulating_func, y0, params_d)
+    rss = calc_simulation_rss(tdata, ydata, simulating_func, y0, k=k)
 
     assert rss == 0.0
 
@@ -39,9 +39,8 @@ def test_non_zero_rss():
 
     ydata = simulating_func(tdata, y0, k)
     ydata[0, 0] += 0.1  # Introduce a small error
-    params_d = {'k': k}
 
-    rss = calc_simulation_rss(tdata, ydata, simulating_func, y0, params_d)
+    rss = calc_simulation_rss(tdata, ydata, simulating_func, y0, k=k)
 
     assert rss > 0.0
 
@@ -61,9 +60,8 @@ def test_rss_calculation_with_mock(sim_return, ydata):
     tdata = np.array([0, 1, 2])
     ydata = np.array(ydata)  # Introduce a small error
     y0 = np.array([1.0, 0.0])
-    params_d = {'k': 1}
 
-    rss = calc_simulation_rss(tdata, ydata, mock_simulating_func, y0, params_d)
+    rss = calc_simulation_rss(tdata, ydata, mock_simulating_func, y0, k=1)
 
     assert rss == np.sum((ydata - mock_simulating_func.return_value)**2)
 
@@ -82,11 +80,60 @@ def test_ydata_with_row_of_nan():
          [0.1, 0.1]])
     
     y0 = np.array([0.0, 0.0])
-    params_d = {'k': 1}
 
-    rss = calc_simulation_rss(tdata, ydata, mock_sim_func, y0, params_d)
+    rss = calc_simulation_rss(tdata, ydata, mock_sim_func, y0, k=1)
 
     assert rss == 0.1**2 * 5
+
+
+def test_use_for_basin_hopping():
+    # A -> B
+    def ode_rhs(t, y, k):
+        return np.array([-k * y[0], k * y[0]])
+
+    simulating_func = make_simulating_func_from_ode_rhs(ode_rhs)
+
+    tdata = np.logspace(-3, 1, 12)
+    y0 = np.array([1, 0])
+
+    def f(x):
+        k = x[0]
+        return calc_simulation_rss(
+            tdata=np.logspace(-3, 1, 12), 
+            ydata=simulating_func(tdata, y0, k),
+            simulating_func=simulating_func,
+            y0=y0,
+            k=k
+        )
+
+    result = basinhopping(f, [0.0])
+
+    assert result.fun == 0.0
+
+
+def test_use_for_differential_evolution():
+    # A -> B
+    def ode_rhs(t, y, k):
+        return np.array([-k * y[0], k * y[0]])
+
+    simulating_func = make_simulating_func_from_ode_rhs(ode_rhs)
+
+    tdata = np.logspace(-3, 1, 12)
+    y0 = np.array([1, 0])
+
+    def f(x):
+        k = x[0]
+        return calc_simulation_rss(
+            tdata=np.logspace(-3, 1, 12), 
+            ydata=simulating_func(tdata, y0, k),
+            simulating_func=simulating_func,
+            y0=y0,
+            k=k
+        )
+
+    result = differential_evolution(f, bounds=[(0, 10)])
+
+    assert result.fun == 0.0
 
 
 if __name__ == '__main__':
